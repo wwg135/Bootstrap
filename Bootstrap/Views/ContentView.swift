46e55e252be6d8713be66ae231fa673582bc6d56
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-01-05 10:34:57 +0800

    Update ContentView.swift

--- Bootstrap/Views/ContentView.swift
+++ Bootstrap/Views/ContentView.swift
@@ -17,8 +17,6 @@ struct BootstrapView: View {
     @State var openSSH = false
     @State var showOptions = false
     @State var showCredits = false
-    @State var updateAvailable = false
-    @State var versionRegex = try? NSRegularExpression(pattern: "\\d+\\.\\d+\\.\\d+")
     let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
     
     var body: some View {
@@ -90,26 +88,6 @@ struct BootstrapView: View {
                             }
                             .disabled(isSystemBootstrapped())
                         }
-
-                        if updateAvailable {
-                            Button {
-                                let link = "https://github.com/wwg135/Bootstrap/releases"
-                                if let url = URL(string: link) {
-                                    UIApplication.shared.open(url)
-                                }
-                            } label: {
-                                Label(
-                                    title: { Text("Button_Update_Available") },
-                                    icon: { Image(systemName: "arrow.down.circle") }
-                                )
-                                .padding(25)
-                            }
-                            .background {
-                                Color(UIColor.systemBackground)
-                                    .cornerRadius(20)
-                                    .opacity(0.5)
-                            }
-                        }
                     
                         HStack {
                             Button {
@@ -178,6 +156,20 @@ struct BootstrapView: View {
                         Text("UI made with love by haxi0. ♡")
                             .font(Font.system(size: 13))
                             .opacity(0.5)
+
+                        Button {
+                            let link = "https://github.com/wwg135/Bootstrap/releases"
+                            if let url = URL(string: link) {
+                                UIApplication.shared.open(url)
+                            }
+                        } label: {
+                            Label(
+                                title: { Text("Button_Update_Available") },
+                                icon: { Image(systemName: "arrow.down.circle") }
+                            )
+                            .font(Font.system(size: 15))
+                            .opacity(0.5)
+                        }    
                     }
                 }
 
@@ -204,42 +196,6 @@ struct BootstrapView: View {
                     OptionsView(showOptions: $showOptions, openSSH: $openSSH)
                 }
             }
-        }
-        .onAppear {
-            DispatchQueue.global(qos: .userInitiated).async {
-                Task {
-                    do {
-                        try await checkForUpdates()
-                    } catch {
-                        print("Error: ", error)
-                    }
-                }
-            }
-        }
-    }
-
-    func checkForUpdates() async throws {
-        let currentAppVersion = "AAB"      
-        let releasesURL = URL(string: "https://api.github.com/repos/wwg135/Bootstrap/releases")!
-        let releasesRequest = URLRequest(url: releasesURL)
-        let (releasesData, _) = try await URLSession.shared.data(for: releasesRequest)
-        guard let releasesJSON = try JSONSerialization.jsonObject(with: releasesData, options: []) as? [[String: Any]] else {
-            return
-        }
-
-        if releasesJSON.first(where: {
-            if let version = $0["name"] as? String, versionRegex?.firstMatch(in: version, options: [], range: NSRange(location: 0, length: version.utf16.count)) != nil {   
-                if let latestName = $0["tag_name"] as? String, let latestVersion = $0["name"] as? String {
-                    if latestName.count == 10 && currentAppVersion.count == 10 {
-                        if latestName > currentAppVersion && versionRegex?.firstMatch(in: latestVersion, options: [], range: NSRange(location: 0, length: latestVersion.utf16.count)) != nil {
-                            return true  
-                        }
-                    }
-                }
-            }
-            return false
-        }) != nil {
-            updateAvailable = true
         }
     }
     
